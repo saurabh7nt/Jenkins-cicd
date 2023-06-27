f38a177e045442d348903856527e57719579bb01
Saurabh Tajane <saurabhtajne07@gmail.com>
2023-06-27 16:47:06 +0530

    Initial Commit

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+# Use an official Node.js runtime as the base image
+FROM node:14-alpine
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy package.json and package-lock.json to the working directory
+COPY package*.json ./
+
+# Install project dependencies
+RUN npm install
+
+# Copy the entire project to the working directory
+COPY . .
+
+# Build the React app
+RUN npm run build
+
+# Set environment variables
+ENV PORT=3000
+
+# Expose the specified port
+EXPOSE $PORT
+
+# Run the React app using the production build
+CMD ["npm", "start"]
---
+++ src/Component/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function App() {
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <div className="card" style={{ width: '18rem' }}>
+                <img src="https://images.unsplash.com/photo-1517849845537-4d257902454a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8ZnVubnl8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60" className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">The Hilarious Hound!</h5>
+                    <p className="card-text"> Ladies and gentlemen, meet the dog that's here to brighten your world and leave you in stitches ;)"</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+    );
+}
